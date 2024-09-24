fb6feb7c3d6bd7dc0101362d7b06c60d82f39d8e
Yasunori Shimura <yasu0710@gmail.com>
2024-09-25 08:39:07 +0900

    add missing misc.py

---
+++ misc.py
@@ -0,0 +1,16 @@
+from ascii_j import *
+
+def dump_data(data:bytearray):
+    ascii_buf = ''
+    for count, dt in enumerate(data):
+        if count % 16 == 0:
+            ascii_buf = ''
+            print(f'{count:04x}', end='')
+        print(f' {dt:02x}', end='')
+        ascii_buf += ascii_table_half[dt]
+        if count % 16 == 15:
+            print(f'  {ascii_buf}')
+            ascii_buf = ''
+    if ascii_buf != '':
+        print('   ' * (16-(count % 16)))
+        print(f'  {ascii_buf}')
