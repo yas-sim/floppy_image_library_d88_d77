0fa60921bc25036849ef1038628c03f03a39c9d4
Yasunori Shimura <yasu0710@gmail.com>
2024-10-03 11:27:26 +0900

    add lacking __init__.py

---
+++ fdimagelib/__init__.py
@@ -0,0 +1,6 @@
+from fdimagelib.floppy_image import *
+from fdimagelib.fbasic_utils import *
+from fdimagelib.file_system import *
+from fdimagelib.ascii_j import *
+from fdimagelib.misc import *
+from fdimagelib.motorola_s import *
